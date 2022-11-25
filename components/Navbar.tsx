7c1853cf6c6b71887d7d240e1982d65790b0c16f
Vincent Fleming <flemingvincent@Vincents-Mac-Studio.local>
2022-11-25 10:22:13 -0500

    adding github and linkedin links to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Link from "next/link";
 
 import Logo from "../assets/fvlogo.svg";
+import Github from "../assets/github.svg";
+import Linkedin from "../assets/linkedin.svg";
 import { Bars4Icon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
@@ -12,7 +14,7 @@ const Navbar = () => {
         <Bars4Icon className="h-6 w-6" />
       </button>
       <div className="hidden lg:flex text-sm font-mono text-white">
-        <div className="border-r border-r-black space-x-8">
+        <div className="border-r border-r-white space-x-8">
           <Link href="/">// Home</Link>
           <Link href="/technologies">// Technologies</Link>
           <Link href="/work">// Work</Link>
@@ -20,6 +22,17 @@ const Navbar = () => {
           <Link href="/contact" className="pr-8">
             // Contact
           </Link>
+        </div>
+        <div className="flex flex-row space-x-4">
+          <a
+            className="pl-4 cursor-pointer"
+            href="https://github.com/FlemingVincent"
+          >
+            <Github />
+          </a>
+          <a href="https://www.linkedin.com/in/vincentfleming">
+            <Linkedin />
+          </a>
         </div>
       </div>
     </nav>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,12 @@ const Home: NextPage = () => {
       <main className="w-full h-screen px-6 bg-[#010101] text-white">
         <Navbar />
         <div className="flex flex-col items-center justify-center h-[calc(100vh-40px)]">
-          <h1></h1>
-          <h1 className="font-inter font-bold text-4xl lg:text-8xl inline-flex items-center">
+          <h1 className="font-inter font-bold text-4xl md:text-6xl lg:text-8xl inline-flex items-center">
             Vincent Fleming{" "}
-            <div id="cursor" className="w-4 h-10 lg:w-8 lg:h-20"></div>
+            <div
+              id="cursor"
+              className="w-4 h-10 md:w-6 md:h-[60px] lg:w-8 lg:h-20"
+            ></div>
           </h1>
         </div>
       </main>
