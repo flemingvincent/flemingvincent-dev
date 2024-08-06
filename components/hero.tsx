2db82e7c31d37b2c52f80176eb0cb250be277893
Vincent Fleming <flemingvincent7@gmail.com>
2024-08-06 09:47:55 -0400

    Updated content.

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,18 +3,13 @@ export function Hero() {
 		<div className="space-y-4">
 			<p className="font-semibold">Today</p>
 			<p className="text-neutral-600 dark:text-neutral-400">
-				I&apos;m a technology analyst at Dun & Bradstreet, where I work on
-				technical projects including full-stack development, machine learning,
-				and more.
+				Currently, I&apos;m a technology analyst at Dun & Bradstreet, where I
+				work on technical projects including full-stack development, machine
+				learning, and more.
 			</p>
 			<p className="text-neutral-600 dark:text-neutral-400">
 				Additionally, I freelance as a software engineer, specializing in web
 				and mobile application development with React and React Native.
-			</p>
-			<p className="text-neutral-600 dark:text-neutral-400">
-				Previously, I was a software engineer at Flyjets, where I developed and
-				maintained an iOS, android, and web application for a charter aviation
-				marketplace.
 			</p>
 		</div>
 	);
